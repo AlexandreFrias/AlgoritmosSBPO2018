741e837e65cf63081a1459d54a5ec600e1037ae7
AlexandreFrias <alexandrefrias1@hotmail.com>
2018-06-07 01:27:54 -0300

    modificação no arquivo gerador.cpp 06/06/2018

--- gerador.cpp
+++ gerador.cpp
@@ -29,26 +29,35 @@ void cria_instancia(char nomearq[], int n, int k, int p){
 
 int main(){
 
-  srand(time(NULL));
+ 	//srand(time(NULL));
 
-  int N=5; //número de instâncias com uma característica ()
+ 	int N=5; //número de instâncias com uma característica ()
 	int i=1;
 	char nome_arquivo[25];
 	int n[]={100, 200, 400, 800}; //cardinalidade do conjunto de vetores
-  //int k=3 
-	int k[]={3,4,5,6}; //número de subconjuntos da partição
+  int k=3; // manualmente fazendo k=3, 4, 5, 6 sem usar srand() 
+	//int k[]={3,4,5,6}; //número de subconjuntos da partição
 	int p=12; //precisão: número de algarismos máximo dos elementos
 
   
-  for(int i0=0; i0<4; i0++){
-    for(int i1=0; i1<4; i1++){
-      for(int i2=0; i2<N; i2++){
-        sprintf(nome_arquivo, "inst%i.txt", i);
-        cria_instancia(nome_arquivo, n[i1], k[i2], p);
-        i++;
-      }
-    }
-  }
-	
+  // for(int i0=0; i0<4; i0++){
+  //   for(int i1=0; i1<4; i1++){
+  //     for(int i2=0; i2<N; i2++){
+  //       sprintf(nome_arquivo, "inst%i.txt", i);
+  //       cria_instancia(nome_arquivo, n[i1], k[i2], p);
+  //       i++;
+  //     }
+  //   }
+  // }
+
+
+	for(int i0=0; i0<4; i0++){
+     	for(int i1=0; i1<N; i1++){
+        	sprintf(nome_arquivo, "inst%i.txt", i);
+        	cria_instancia(nome_arquivo, n[i0], k, p);
+        	i++;
+    	}
+  	}
+
 	return 0;
 }
